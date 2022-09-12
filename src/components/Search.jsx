19e03e193e8d6f4bd4b03b5da293e00cc065849a
Alex S <qwshao@uwaterloo.ca>
2022-09-12 10:52:49 -0400

    added searching with debounce value, will add jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDebounce } from 'use-debounce';
+import { useResults } from '../contexts/ResultContext';
 import { Options } from './Options';
 
 export const Search = () => {
+  const [ text, setText ] = useState("catto");
+  const { setSearchTerm } = useResults();
+
+  // optimize debounce value for delay, so api doesn't get called on every keystroke
+  const [ debouncedValue ] = useDebounce(text, 500);
+
+  useEffect(() => {
+    if (debouncedValue) {
+      setSearchTerm(debouncedValue);
+    }
+  }, [debouncedValue]);
+
   return (
     <div>
       Search
