c964af649fbfbf385f1360d1ee2d20586ae3002a
Alex S <qwshao@uwaterloo.ca>
2022-09-12 10:52:37 -0400

    fixed margins

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -12,7 +12,7 @@ export const Options = () => {
   return (
     <div className="flex sm:justify-around justify-between items-center mt-4">
       {options.map(({ url, text }, index) => (  
-        <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">
+        <NavLink to={url} className="m-2" activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">
           {text}
         </NavLink>
       ))}
